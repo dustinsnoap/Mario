a123af20951d79f8b5634a1bb2ed52c1ac223b05
Dustin Snoap <afrobacon@gmail.com>
2019-03-22 23:39:30 -0400

    Added brick svg

--- mario/src/App.js
+++ mario/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './reset.css'
 import './App.css';
+import Bricks from './assets/blocks/bricks.svg'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Mario</h1>
+        <svg viewBox="0 0 16 16" alt="brick" height="160" width="160">
+          <use xlinkHref={Bricks + "#brick"} />
+        </svg>
       </div>
     );
   }
