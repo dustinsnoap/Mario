fcbc3c504fb7990383640226e0117cfea39679f6
Dustin Snoap <afrobacon@gmail.com>
2019-03-27 23:28:03 -0400

    Now with graphics

--- mario/src/App.js
+++ mario/src/App.js
@@ -4,12 +4,40 @@ import './App.css';
 import Bricks from './assets/blocks/bricks.svg'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      screen: [],
+    }
+  }
+  componentDidMount() {
+    let col = [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,0,0,0];
+    let screen = [col,col,col,col,col,col,col,col,col,col,col,col,col,col,col,col,]
+    let test = this.fillScreen(screen)
+    this.setState({screen: test})
+  }
+  fillScreen = screenArray => {
+    return screenArray.map(row => row.map(blockID => this.getBlock(blockID)))
+  }
+  getBlock = blockID => {
+    let blockName = ''
+    switch(blockID) {
+      case 0: blockName = 'ground'; break
+      case 1: blockName = 'brick'; break
+      case 2: blockName = 'question'; break
+      default: blockName = 'none'; break
+    }
+    return (
+      <svg viewBox="0 0 16 16" alt="brick" height="160" width="160">
+        <use xlinkHref={Bricks + '#' + blockName} />
+      </svg>
+    )
+  }
   render() {
     return (
       <div className="App">
-        <svg viewBox="0 0 16 16" alt="brick" height="160" width="160">
-          <use xlinkHref={Bricks + "#brick"} />
-        </svg>
+        {this.state.screen.map(row => 
+          <div>{row.map(block => block)}</div>)}
       </div>
     );
   }
